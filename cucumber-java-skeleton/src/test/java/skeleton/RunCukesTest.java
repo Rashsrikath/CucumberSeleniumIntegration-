00f3b4635a08fe413c7e806285261f386ca351d6
Training_B8C.07.19 <Training_B8C.07.19@BDC8-DX-431JX52.dir.svc.accenture.com>
2018-07-19 12:39:43 +0530

    updated

--- cucumber-java-skeleton/src/test/java/skeleton/RunCukesTest.java
+++ cucumber-java-skeleton/src/test/java/skeleton/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty","html:src/cucumber-reports","json:target/cucumber-reports/reports.json"},tags= {"@Regression"},monochrome=true)
+@CucumberOptions(plugin = {"pretty","html:src/cucumber-reports","json:src/cucumber-reports/reports.json"},tags= {"@Regression"},monochrome=true)
 public class RunCukesTest {
 }
